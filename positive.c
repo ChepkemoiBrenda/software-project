36096f4b6ea37a74e422c094c8d434dacadbc95d
ChepkemoiBrenda <chepkemoibree1@gmail.com>
2025-02-25 17:53:25 +0300

    Add simple positive.cc file

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	    double num;
+
+	        // Ask the user to enter a number
+		//     printf("Enter a number: ");
+		//         scanf("%lf", &num);
+		//
+		//             // Check if the number is positive
+		//                 if (num > 0) {
+		//                         printf("%.2lf is a positive number.\n", num);
+		//                             } else {
+		//                                     printf("%.2lf is not a positive number.\n", num);
+		//                                         }
+		//
+		//                                             return 0;
+		//                                             }
+		//
